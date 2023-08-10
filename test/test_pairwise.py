c6d5eb54da6aad17a36295b27dec9b9181808d83
Nick <129078463+nicholasjmorris1993@users.noreply.github.com>
2023-08-10 18:13:41 -0400

    Add files via upload

--- test/test_pairwise.py
+++ test/test_pairwise.py
@@ -1,1 +1,17 @@
+import pandas as pd
+import sys
+sys.path.append("/home/nick/Pairwise-Metrics/src")
+from pairwise import distances
 
+
+data = pd.read_csv("/home/nick/Pairwise-Metrics/test/LungCap.csv")
+
+metrics = distances(data)
+
+metrics.correlation
+
+metrics.covariance
+
+metrics.euclidean
+
+metrics.cosine
